9df9b89df834349b321a8cd6ac17fc3ea6c68faa
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-06-27 11:07:11 -0400

    clases llenas de entidades

--- Registro con Detalle 2-2020/Entidades/Articulos.cs
+++ Registro con Detalle 2-2020/Entidades/Articulos.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Registro_con_Detalle_2_2020.Entidades
 {
-    class Articulos
-    {
+        public class Articulos
+        {
+            [Key]
+            public int IdArticulo { get; set; }
+            public string Descripcion { get; set; }
+            public int Existencia { get; set; }
+            public double Costo { get; set; }
+            public double ValorInventario { get; set; }
+
+            //REGISTRO DETALLADO
+            [ForeignKey("IdArticulo")]
+            public List<ArticulosDetalle> Detalle { get; set; } = new List<ArticulosDetalle>();
+        }
     }
-}
--- Registro con Detalle 2-2020/Entidades/ArticulosDetalle.cs
+++ Registro con Detalle 2-2020/Entidades/ArticulosDetalle.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Registro_con_Detalle_2_2020.Entidades
 {
-    class ArticulosDetalle
+    public class ArticulosDetalle
     {
+        [Key]
+        public int Id { get; set; }
+        public int IdArticulo { get; set; }
+        public string Requerimiento { get; set; }
+        public float Valor { get; set; }
+        public ArticulosDetalle(int idArticulo, string requerimiento, float valor)
+        {
+            Id = 0;
+            IdArticulo = idArticulo;
+            Requerimiento = requerimiento;
+            Valor = valor;
+        }
     }
 }
